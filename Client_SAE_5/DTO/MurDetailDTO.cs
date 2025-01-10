b42dac8b93026affb542f874aa991eff8ed9221d
Dorian GEHANT <gehantd@iut-acy.local>
2025-01-10 15:13:46 +0100

    Mise à jour page unite et mur

--- Client_SAE_5/DTO/MurDetailDTO.cs
+++ Client_SAE_5/DTO/MurDetailDTO.cs
@@ -16,15 +16,25 @@ namespace Client_SAE_5.DTO
 
         public int IdSalle { get; set; }
 
-        public decimal Longueur { get; set; } = 0;
+        public decimal Longueur { get; set; }
 
-        public decimal Hauteur { get; set; } = 0;
+        public decimal Hauteur { get; set; }
 
-        public decimal Orientation { get; set; } = 0;
+        public decimal Orientation { get; set; }
 
         public SalleSansNavigationDTO? SalleNavigation { get => salleNavigation; set => salleNavigation = value; }
         public DirectionSansNavigationDTO? DirectionNavigation { get => directionNavigation; set => directionNavigation = value; }
         public virtual ICollection<CapteurSansNavigationDTO> Capteurs { get => capteurs; set => capteurs = value; }
         public virtual ICollection<EquipementSansNavigationDTO> Equipements { get => equipements; set => equipements = value; }
+
+        public MurDetailDTO()
+        {
+            IdMur = 0;
+            IdDirection = 0;
+            IdSalle = 0;
+            Longueur = 0;
+            Hauteur = 0;
+            Orientation = 0;
+        }
     }
 }
